30ba9ac88044cc63cf68095c8bf1dff11f3b3597
thomaslandry1 <thomas.landry1@gmail.com>
2017-05-24 13:27:16 -0700

    Update Complex.cpp

--- Complex.cpp
+++ Complex.cpp
@@ -3,61 +3,73 @@
 //
 
 #include "Complex.h"
-#include <math.h>
+#include <math.h>        // fabs used in ostream operator
 #include <iostream>
 #include <iomanip>
 
 using std::setw;
+using std::cout;
+const int PRECISION = 1;
 const int width = 5;
 
-Complex::Complex(double a, double b) : RealPart_(a), ImaginaryPart_(b) {
+Complex::Complex(double a, double b) : Real_(a), Imag_(b) {
 }
 
 Complex::~Complex() {
 }
 
 Complex::Complex(const Complex& copy)
-    : RealPart_(copy.RealPart_), ImaginaryPart_(copy.ImaginaryPart_) {
+    : Real_(copy.Real_), Imag_(copy.Imag_) {
 }
 
 std::ostream& operator<<(std::ostream& os, const Complex& rhs) {
-  os << setw(width) << rhs.RealPart_;
-  if (rhs.ImaginaryPart_ > 0) {
+  cout.setf(std::ios::fixed);
+  cout.precision(PRECISION);
+
+  os << setw(width) << rhs.Real_;
+
+  //checks to see if number is positive or neg
+
+  if (rhs.Imag_ > 0) {
     os << " + ";
   } else {
     os << " - ";
   }
-  os << setw(width) << fabs(rhs.ImaginaryPart_) << rhs.ImaginaryUnit_;
+
+  // outputs imaginary part along with unit -> "14i"
+  os << setw(width) << fabs(rhs.Imag_) << rhs.ImaginaryUnit_;
+
   return os;
 }
 
 const Complex operator+(const Complex& lhs, const Complex& rhs) {
-  return Complex(lhs.RealPart_ + rhs.RealPart_,
-                 lhs.ImaginaryPart_ + rhs.ImaginaryPart_);
+  return Complex(lhs.Real_ + rhs.Real_,
+                 lhs.Imag_ + rhs.Imag_);
 }
 
 const Complex operator-(const Complex& lhs, const Complex& rhs) {
-  return Complex(lhs.RealPart_ - rhs.RealPart_,
-                 lhs.ImaginaryPart_ - rhs.ImaginaryPart_);
+  return Complex(lhs.Real_ - rhs.Real_,
+                 lhs.Imag_ - rhs.Imag_);
 }
 
 const Complex operator*(const Complex& lhs, const Complex& rhs) {
-  double ac = lhs.RealPart_ * rhs.RealPart_;
-  double bd = lhs.ImaginaryPart_ * rhs.ImaginaryPart_;
-  double bc = lhs.ImaginaryPart_ * rhs.RealPart_;
-  double ad = lhs.RealPart_ * rhs.ImaginaryPart_;
+  double ac = lhs.Real_ * rhs.Real_;
+  double bd = lhs.Imag_ * rhs.Imag_;
+  double bc = lhs.Imag_ * rhs.Real_;
+  double ad = lhs.Real_ * rhs.Imag_;
 
   return Complex ( (ac-bd), (bc+ad));
 }
 
 const Complex operator/(const Complex& lhs, const Complex& rhs) {
-  double ac = lhs.RealPart_ * rhs.RealPart_;
-  double bd = lhs.ImaginaryPart_ * rhs.ImaginaryPart_;
-  double bc = lhs.ImaginaryPart_ * rhs.RealPart_;
-  double ad = lhs.RealPart_ * rhs.ImaginaryPart_;
-  double c2 = rhs.RealPart_ * rhs.RealPart_;
-  double d2 = rhs.ImaginaryPart_ * rhs.ImaginaryPart_;
+  double ac = lhs.Real_ * rhs.Real_;
+  double bd = lhs.Imag_ * rhs.Imag_;
+  double bc = lhs.Imag_ * rhs.Real_;
+  double ad = lhs.Real_ * rhs.Imag_;
+  double c2 = rhs.Real_ * rhs.Real_;
+  double d2 = rhs.Imag_ * rhs.Imag_;
 
   return Complex((ac+bd)/(c2+d2), (bc-ad)/(c2+d2));
 }
+
 
