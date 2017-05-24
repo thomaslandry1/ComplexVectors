180e8bf7e667414777be440e83c1fbf056f1669d
thomaslandry1 <thomas.landry1@gmail.com>
2017-05-24 13:28:37 -0700

    Update ComplexVector.cpp

--- ComplexVector.cpp
+++ ComplexVector.cpp
@@ -15,59 +15,47 @@ using std::setw;
 const int PRECISION = 1;
 const int WIDTH = 2;
 
-ComplexVector::ComplexVector() { }
+ComplexVector::ComplexVector() {
+}
 
-ComplexVector::ComplexVector(Complex& num) : VectorParts_( {num} ) { }
-
-ComplexVector::ComplexVector(double a, double b, double c, double d, double e, double f, double g, double h)
-    : VectorParts_({ Complex(a, b), Complex(c, d), Complex(e, f),
-                     Complex(g, h) })
-{ }
-
-ComplexVector::~ComplexVector() {
+ComplexVector::ComplexVector(double a, double b, double c, double d,
+                             double e, double f, double g, double h)
+    : VectorParts_({ Complex(a, b), Complex(c, d),
+                     Complex(e, f), Complex(g, h) }) {
 }
 
 ComplexVector::ComplexVector(const ComplexVector& copy)
     : VectorParts_(copy.VectorParts_) {
 }
 
-void ComplexVector::computeSequence(int n) {
-  std::ofstream outFile("ComplexSequence.txt", std::ios::app);
-  if (n == 0) {
-    VectorParts_.push_back( (1,1) );
-  }
-  Complex num( 2*(n+1), 3*(n+1) );
-  Complex den( 7, 5*((n+1)*(n+1)));
-  Complex result = num / den * VectorParts_[n];
-  if (n < 5) {
-    VectorParts_.push_back(result);
-    computeSequence(n + 1);
-  }
-
-  if (n == 5) {
-    for (auto i : VectorParts_) {
-      outFile << i << endl;
-    }
-  }
+ComplexVector::~ComplexVector() {
 }
 
 void ComplexVector::print() const {
   auto& end = *(VectorParts_.rbegin());
 
-  cout << " <";
+  cout << " <" << *this << " >\n";
+}
 
-  cout.setf(std::ios::fixed);
-  cout.precision(PRECISION);
+void ComplexVector::computeSequence(int n) {
+  std::ofstream outFile("ComplexSequence.txt", std::ios::app);
 
-  for (auto& i : VectorParts_) {
-    cout << setw(WIDTH) << i;
+  // base case
+  if (n == 0) {
+    VectorParts_.push_back(Complex(1, 1));
+  }
 
-    if (&i != &end) {
-      cout << setw(WIDTH) << " , ";
+  //recursive sequence setup
+  Complex num(Areal_ * (n + 1), Bimag_ * (n + 1));
+  Complex den(Creal_, Dimag_ * ((n + 1) * (n + 1)));
+  Complex result = num / den * VectorParts_[n];
 
-    } else {
-      cout << " >\n";
-    }
+  if (n != 5) {
+    VectorParts_.push_back(result);
+    computeSequence(n + 1);
+  } else {
+    outFile << "{" << *this << " } ";
+    outFile.close();
   }
 }
 
@@ -113,4 +101,26 @@ const ComplexVector operator/(const ComplexVector& lhs, const ComplexVector& rhs
     ++rhsIter;
   }
   return newVector;
+}
+
+std::ostream& operator<<(std::ostream& os, const ComplexVector& rhs) {
+
+  //iterator to reverse beginning of vector
+  //needed because ranged based for loops unable to offer index of current
+  auto& end = *(rhs.VectorParts_.rbegin());
+
+  cout.setf(std::ios::fixed);
+  cout.precision(PRECISION);
+
+  for (auto& i : rhs.VectorParts_) {
+    os << setw(WIDTH) << i;
+
+    if (&i != &end) {
+      os << setw(WIDTH) << " , ";
+
+    } else {
+      os << "";
+    }
+  }
+  return os;
 }
